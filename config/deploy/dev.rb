ca6f596171ac65f7b3732ede9c99e7d46c5fb644
toubi@ishtar <antoine@toubhans.org>
2016-01-08 10:51:26 +0100

    deploy

--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -1,9 +1,9 @@
-set :branch, 'dev'
+set :branch, 'master'
 set :sudo, 'false'
 set :ssh_user, 'ubuntu'
-set :deploy_to, '/var/www/myapp/current/'
+# set :deploy_to, '/var/www/demo/'
 
 server '62.4.23.202', user: fetch(:ssh_user)
-SSHKit.config.command_map[:composer] = "php #{shared_path.join("composer.phar")}"
+# SSHKit.config.command_map[:composer] = "php #{shared_path.join("composer.phar")}"
 
-load 'config/deploy/alkante_tasks.rb'
+
