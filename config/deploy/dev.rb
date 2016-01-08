2d1793f374aa02954911d92e1bf043e9c50f96a1
toubi@ishtar <antoine@toubhans.org>
2016-01-08 18:47:41 +0100

    ras

--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -2,7 +2,7 @@ set :branch, 'master'
 set :sudo, 'false'
 # set :deploy_to, '/var/www/demo/'
 
-#set :symfony_env,  "dev"
+set :symfony_env,  "dev"
 set :controllers_to_clear, []
 
 set :ssh_user, 'ubuntu'
