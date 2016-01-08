b5f94309e1f181d2125895dba2349d91646ba5c9
toubi@ishtar <antoine@toubhans.org>
2016-01-08 11:13:11 +0100

    deploy

--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -1,8 +1,8 @@
 set :branch, 'master'
 set :sudo, 'false'
-set :ssh_user, 'ubuntu'
 # set :deploy_to, '/var/www/demo/'
 
+set :ssh_user, 'ubuntu'
 server '62.4.23.202', user: fetch(:ssh_user)
 # SSHKit.config.command_map[:composer] = "php #{shared_path.join("composer.phar")}"
 
