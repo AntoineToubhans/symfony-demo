7ca92bcd377cc60775a483c4179733baacbbcb9c
toubi@ishtar <antoine@toubhans.org>
2016-01-08 11:25:35 +0100

    controller clear

--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -2,8 +2,10 @@ set :branch, 'master'
 set :sudo, 'false'
 # set :deploy_to, '/var/www/demo/'
 
+set :controllers_to_clear, []
+
 set :ssh_user, 'ubuntu'
 server '62.4.23.202', user: fetch(:ssh_user)
-# SSHKit.config.command_map[:composer] = "php #{shared_path.join("composer.phar")}"
+
 
 
