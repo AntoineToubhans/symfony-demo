980c1e320bc98261a82bb8ade87e534e7582bb48
toubi@ishtar <antoine@toubhans.org>
2016-01-10 14:33:05 +0100

    ip savigny

--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -2,9 +2,10 @@ set :branch, 'master'
 set :sudo, 'false'
 # set :deploy_to, '/var/www/demo/'
 
-set :symfony_env,  "dev"
+#set :symfony_env,  "dev"
 set :controllers_to_clear, []
 
 set :ssh_user, 'ubuntu'
-server '62.4.23.202', user: fetch(:ssh_user)
+#server '62.4.23.202', user: fetch(:ssh_user)
+server 'formation-openstack', user: fetch(:ssh_user)
 
